a6e14993f0b12f691e90b47914a1b50ee1a52c62
Robson Carvalho <robson73904@gmail.com>
2022-01-26 20:58:37 -0300

    atualização no código javascript

--- js/toggle.js
+++ js/toggle.js
@@ -1,16 +1,19 @@
 const body = document.body;
 const ball = document.querySelector(".ball");
-localStorage.setItem("theme", "light");
+const theme = {
+  value: "light",
+};
 
 const changeTheme = () => {
-  if (localStorage.theme == "light") {
+  if (theme.value == "light") {
     body.style.backgroundColor = "var(--dark)";
     body.style.color = "var(--light)";
-    localStorage.setItem("theme", "dark");
-  } else if (localStorage.theme == "dark") {
+    theme.value = "dark";
+  } else if (theme.value == "dark") {
     body.style.backgroundColor = "var(--light)";
     body.style.color = "var(--dark)";
-    localStorage.setItem("theme", "light");
+
+    theme.value = "light";
   }
 };
 
